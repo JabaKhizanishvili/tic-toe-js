3a2b79fae73e83de42c4d016405022f651d62429
xizzana <xizanishvili.99@gmail.com>
2021-12-19 14:45:53 +0400

    initial code

--- main.js
+++ main.js
@@ -14,9 +14,8 @@
 let box = document.querySelectorAll(".box");
 let counter = 0;
 
-let obj = [];
-
-box.forEach(function(e,key){
+let logic = true;
+box.forEach(function(e){
     e.addEventListener("click", ()=>{
         // for (let i=0; i < box.length; i++){
         //     e.innerHTML = "<i class='far fa-circle'></i>";
@@ -28,18 +27,20 @@ box.forEach(function(e,key){
 
         // counter++;
         // if(counter %2 == 0) {
-        //     e.innerHTML = "<i class='far fa-circle'></i>";
-        // }else{
         //     e.innerHTML = "<i class='fas fa-times'></i>";
+        // }else{
+        //     e.innerHTML = "<i class='fas fa-circle'></i>";
         // }
+       if(e.innerHTML == ""){
+        if(logic){
+            e.innerHTML = "<i class='fas fa-times'></i>";
+           } else{
+            e.innerHTML = "<i class='far fa-circle'></i>";
+           }
+    
+           logic = !logic;
+       }
        
-        if(key % 2 == 0) e.innerHTML = "<i class='far fa-circle'></i>"
-        else e.innerHTML = "<i class='fas fa-times'>"
-   
-        obj.push([key, e]);
-        console.log(obj)
-
-        
     })
 })
 
